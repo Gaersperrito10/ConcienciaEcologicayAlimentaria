a552b9b3bb28b670fbb8bad6dccf7d602a2a8673
Gaersperrito10 <richyerbarod12@gmail.com>
2024-06-15 23:05:22 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+document.addEventListener("DOMContentLoaded", function() {
+    alert("¡Bienvenido a la página de Estilo de vida Saludable! Diviertete e informate acerca de nuestra comunidad, cambios en nuestra dieta y mucho mas por explorar");
+    });
